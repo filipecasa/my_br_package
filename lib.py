c0ad798616b9af58017de81ec999c9f57a5c9fe7
filipecasa <filipecasa@gmail.com>
2021-08-16 14:28:26 +0200

    Initial Push

---
+++ lib.py
@@ -0,0 +1,6 @@
+def try_me(num, num_1):
+
+    if type(num) == str or type(num_1) == str:
+        return "REASON FOR EXCLUSION"
+
+    return num + num_1
--- try_me.py
+++
@@ -1,6 +0,0 @@
-def sum(num, num_1):
-
-    if type(num) == string or type(num_1) == string:
-        return "REASON FOR EXCLUSION"
-
-    return num + num_1
